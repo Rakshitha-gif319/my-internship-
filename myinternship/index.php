a2aedc0864cd8fabbac38c095469f81ce342c9da
Challaram Rakshitha <rakshithachallaram@gmail.com>
2025-07-25 20:55:40 +0530

    Added index.php for Task 1

---
+++ myinternship/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to My Internship</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', sans-serif;
+            background-color: #f0f8ff;
+            color: #333;
+            text-align: center;
+            padding-top: 100px;
+        }
+        h1 {
+            color: #007bff;
+        }
+        p {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+
+   <h1>👋 Hello, ApexPlanet!</h1>
+<p>Welcome to my Web Development Internship</p>
+<p><strong>Task 1:</strong> Environment successfully set up!</p>
+<p>Intern: <strong>Rakshitha Challaram</strong></p> <!-- Your name here -->
+<p>🖥️ PHP + MySQL + VS Code + GitHub</p>
+
+</body>
+</html>
